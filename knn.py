abfd702102d736e515d9235352876d616c296d40
diaozhida <44797042+diaozhida@users.noreply.github.com>
2018-11-06 14:36:40 +0800

    更新knn算法
    添加knn算法的入参

--- knn.py
+++ knn.py
@@ -1,2 +1,8 @@
-def classify():
+def classify(inX,dataSet,labels,k):
+  '''k-近邻算法：inX分类的输入值，dataSet：输入的训练样本集，labels：标签，k最近邻居的数目'''
   pass
+
+def createDataSet():
+  group =np.array([1.0,1.1],[1.0,1.0],[0,0.1])
+  labels=['A','A','B','B']
+  return group,labels
